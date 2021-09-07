f3b8a28e0888650a974e17a06ef4bc5e880c3a7f
ShinNouzen <85594230+ShinNouzen@users.noreply.github.com>
2021-09-07 17:56:14 +0530

    Update ship.ts

--- src/commands/Fun/ship.ts
+++ src/commands/Fun/ship.ts
@@ -21,9 +21,9 @@ export default class Command extends BaseCommand {
         const per = Math.floor(Math.random() * 100)
 
 if (per < 25) { 
-var sentence = `${per}% Worse than average ♦️`
+var sentence = `${per}% Worse than average 🙂`
 } else if (per < 50) {
-var sentence = `${per}% I don't know how i feel about it ❇️` 
+var sentence = `${per}% I don't know how i feel about it, but you can givie a try 😀` 
 } else if (per < 75) {
 var sentence = `${per}% Good, I guess... ⭐️` 
 } else if (per < 90) {
