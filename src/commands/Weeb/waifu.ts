84d1958c50e1ded6dc0c41c556899bfb567a10ee
ShinNouzen <85594230+ShinNouzen@users.noreply.github.com>
2021-09-07 10:53:21 +0530

    Add files via upload

---
+++ src/commands/Weeb/waifu.ts
@@ -0,0 +1,31 @@
+import { MessageType } from '@adiwajshing/baileys'
+import MessageHandler from '../../Handlers/MessageHandler'
+import BaseCommand from '../../lib/BaseCommand'
+import request from '../../lib/request'
+import WAClient from '../../lib/WAClient'
+import { ISimplifiedMessage } from '../../typings'
+
+export default class Command extends BaseCommand {
+    constructor(client: WAClient, handler: MessageHandler) {
+        super(client, handler, {
+            command: 'waifu',
+            description: 'Will send you random waifu image.',
+            category: 'Weeb',
+            usage: `${client.config.prefix}waifu`
+        })
+    }
+
+    run = async (M: ISimplifiedMessage): Promise<void> => {
+        const onefive = Math.floor(Math.random() * 145) + 1
+
+        return void M.reply(
+            await request.buffer(`https://media.publit.io/file/Twintails/${onefive}.jpg`),
+
+            MessageType.image,
+            undefined,
+            undefined,
+
+            `*Here you go🌟*`
+        )
+    }
+}
