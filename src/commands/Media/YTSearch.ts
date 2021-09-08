f1aed53a7c4f8b2cf46091601755a11c34b0d76c
ShinNouzen <85594230+ShinNouzen@users.noreply.github.com>
2021-09-08 19:46:54 +0530

    Update YTSearch.ts

--- src/commands/Media/YTSearch.ts
+++ src/commands/Media/YTSearch.ts
@@ -29,7 +29,7 @@ export default class Command extends BaseCommand {
                 videos[i].author.name
             }\n 📙 *Duration:* ${videos[i].duration}\n📘 *URL:* ${videos[i].url}\n\n`
         }
-        M.reply('🌟 searching...')
+        M.reply('🌟 Searching...')
         this.client
             .sendMessage(M.from, text, MessageType.extendedText, {
                 quoted: M.WAMessage,
@@ -43,6 +43,6 @@ export default class Command extends BaseCommand {
                     }
                 }
             })
-            .catch((reason: any) => M.reply(`❌ an error occupered, Reason: ${reason}`))
+            .catch((reason: any) => M.reply(`✖ An error occurred, Reason: ${reason}`))
     }
 }
